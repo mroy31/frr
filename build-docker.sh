b5992928fcccf0cb33e4b99eea6ce546ab061c94
Mickael Royer <mickael.royer@enac.fr>
2021-01-30 23:16:27 +0100

    [ENAC] Update docker image

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# to install cross compile
+# if you docker container to register/install qemu-user (needed after reboot)
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# if you want to use debian version of qemu-user
+# aptitude install qemu-user-static binfmt-support
+
+# docker buildx create --name cross-compile --use
+# docker buildx ls
+# docker buildx inspect --bootstrap
+
 PYNETEM_VERSION=0.15.0
 
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 \
